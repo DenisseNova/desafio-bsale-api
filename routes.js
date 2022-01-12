7c7e99442268eb4d88f80143768517ae9eb28b7a
Denisse Nova <denisse.nova.v@gmail.com>
2022-01-12 19:46:03 -0300

    routes category

--- routes.js
+++ routes.js
@@ -7,6 +7,38 @@ router.get('/', (req, res) => {
   res.json({ message: 'OK' })
 });
 
+router.get('/category', async (req,res) => {
+  const prods = await models.Category.findAll();
+  return res.json(prods)
+})
+
+router.get('/category/products', async (req,res) => {
+  const prods = await models.Category.findAll({
+    include: [
+      {
+        model: models.Product,
+        limit: 2
+      }
+    ]
+  });
+  return res.json(prods)
+})
+
+router.get('/category/:id/products', async (req,res) => {
+  const categoryId = (req.params.id || null)
+  const limit = Number(req.query.limit) || 6;
+  const offset = Number(req.query.offset) || 0;
+
+  const prods = await models.Product.findAndCountAll({
+    where: {
+      category: categoryId
+    },
+    offset,
+    limit
+  });
+  return res.json(prods)
+});
+
 router.get('/products', async (req, res) => {
   const limit = Number(req.query.limit) || 6;
   const offset = Number(req.query.offset) || 0;
@@ -22,19 +54,6 @@ router.get('/products', async (req, res) => {
   });
   return res.json(prods)
 });
-
-router.get('/category/products', async (req,res) => {
-  
-  const prods = await models.Category.findAll({
-    include: [
-      {
-        model: models.Product,
-        limit: 2
-      }
-    ]
-  });
-  return res.json(prods)
-})
 
 router.get('/product/name/:name', async (req,res) => {
   const name = (req.params.name || '').trim().toUpperCase()
